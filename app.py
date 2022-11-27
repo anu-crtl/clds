53ea2b4ac6749f0589ad3b0fc27e7e12b1644e16
anu-crtl <54746618+anu-crtl@users.noreply.github.com>
2022-11-27 01:06:21 +0100

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def welcome():
-    return "Hello, Lets do some Numerical integral! Go to <a href=/numericalintegralservice/0/3.14159>Integration service</a> "
+    return "Hello, It works! Go to <a href=/numericalintegralservice/0/3.14159>Integration service</a> "
 
 @app.route("/numericalintegralservice/<lower>/<upper>")
 def compute(lower, upper):
@@ -23,7 +23,7 @@ def compute(lower, upper):
     html = "<h3>Numerical Integral Service from {} to {} of abs(sin(x))</h3>".format(lower, upper)
     for i in N_list:
         results.append(intigration.intigration(lower, upper, i))
-    html +="<h4>Iterations : Values</h4>"
+    html +="<h4>Iter : Val</h4>"
     for i in range(len(results)):
         html+="<h4>{} : {}</h4>".format(N_list[i], results[i])
         html+= "{}".format(jsonify(key1= N_list[i], key2=results[i]))
